f465d88cbfc3b83a8a638aaa474d6c3e2247b4c6
William Anderson <williamdananderson@gmail.com>
2024-03-28 20:26:41 -0400

    Implemented Queen Logic.

--- Assets/Scripts/Pieces/Queen.cs
+++ Assets/Scripts/Pieces/Queen.cs
@@ -20,12 +20,48 @@ public class Queen : Piece
 
     public override List<General.PossibleMove> GeneratePossibleMoves()
     {
+        Vector3 targetPosition;
+
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                targetPosition = new Vector3(x, y, Constants.PIECE_Z_INDEX);
+                if (IsValidQuuenMove(targetPosition))
+                {
+                    General.PossibleMove possibleMove = new General.PossibleMove(x, y, this);
+                    possiblePieceMovesList.Add(possibleMove);
+                    //Debug.Log(this + " from x " + this.transform.position.x + " y " + this.transform.position.y + " to x " + targetPosition.x + " y " + targetPosition.y);
+                }
+            }
+        }
+
+        //Debug.Log(this + " has " + possiblePieceMovesList.Count + " possible moves.");
         return possiblePieceMovesList;
     }
     
     public override void MoveAttempt(Vector3 targetPosition)
     {
-        Debug.Log("MoveAttempt Start.");
-        MoveExecutor(targetPosition);
+        //Debug.Log("MoveAttempt Start.");
+        if (IsValidQuuenMove(targetPosition)) MoveExecutor(targetPosition);
+    }
+
+    private bool IsValidQuuenMove(Vector3 targetPosition)
+    {
+        bool returnBool = false;
+        Piece target = TrackingHandler.pieceTracker[(int)targetPosition.x, (int)targetPosition.y];
+        
+        // Combined logic for Rook and Bishop
+        if ((targetPosition.x - transform.position.x == targetPosition.y - transform.position.y ||
+            targetPosition.x - transform.position.x == -1 * (targetPosition.y - transform.position.y) ||
+            targetPosition.x == transform.position.x  || 
+            targetPosition.y == transform.position.y) &&
+            (target == null || !target.CompareTag(this.tag)) &&
+            !IsRangeMoveBlocked(targetPosition))
+        {
+            returnBool = true;
+        }
+
+        return returnBool;
     }
 }
